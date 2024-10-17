4f1bbc0829ab844c2b50a7baaa7a9a77af02c756
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-17 13:25:26 +0200

    ADD esercizio mail

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 //Es. MAIL
 
+const userEmails = ["ciao@prova.it", "test@test.com", "hello@world.com"];
 
+let email = prompt("Inserisci qui la tua email: ");
+
+if(userEmails.includes(email)){
+    console.log(`Benvenuto ${email}`);
+}else{
+    console.log("La tua mail non risulta presente nel nostro elenco.")
+}
 
 //Es. DADI
